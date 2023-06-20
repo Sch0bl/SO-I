f88f943b2e585df6a068e23be4f56b4429a080e9
Luciano Scola <lucianoluigiscola@gmail.com>
2023-06-19 22:24:26 -0300

    Revisado eje 8 labdcc

--- Practica-4/eje_8.c
+++ Practica-4/eje_8.c
@@ -6,15 +6,16 @@ void global_share_sum(int private_data, int id, int num_procs){
 }
 
 int main(int argc, char **argv) {
-  int num_procs, my_id, sum, buff;
+  int num_procs, my_id, sum, buff, sumando;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-    sum = my_id + 1;
+    sumando = my_id;
+    sum = buff = sumando; // 
     if (my_id == 0) {
     for (int i = 0; i < num_procs - 1; i++){
       printf("Soy el proceso %d, mi suma actual es %d y estoy en la iteración %d\n", my_id, sum, i);
-      MPI_Send(&sum, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      MPI_Send(&buff, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
       MPI_Recv(&buff, 1, MPI_INT, num_procs - 1, 0, MPI_COMM_WORLD, NULL);
       sum += buff;
       printf("Total de la suma %d, en la iteración %d, id %d\n", sum, i, my_id);
@@ -23,7 +24,7 @@ int main(int argc, char **argv) {
     } else {
       for (int i = 0; i < num_procs - 1; i++){
         printf("Soy el proceso %d, mi suma actual es %d y estoy en la iteración %d\n", my_id, sum, i);
-        MPI_Send(&sum, 1, MPI_INT, (my_id + 1) % num_procs, 0, MPI_COMM_WORLD);
+        MPI_Send(&buff, 1, MPI_INT, (my_id + 1) % num_procs, 0, MPI_COMM_WORLD);
         MPI_Recv(&buff, 1, MPI_INT,  (my_id - 1) % num_procs, 0, MPI_COMM_WORLD, NULL);
         sum += buff;
       printf("Total de la suma %d, en la iteración %d, id %d\n", sum, i, my_id);
