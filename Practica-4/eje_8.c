7760c799c8cab79ac35279a9e08295e6c2503b75
Luciano Scola <lucianoluigiscola@gmail.com>
2023-06-19 21:53:52 -0300

    Prueba 2, eje 8

--- Practica-4/eje_8.c
+++ Practica-4/eje_8.c
@@ -1,27 +1,35 @@
 #include <mpi.h>
 #include <stdio.h>
 
+void global_share_sum(int private_data, int id, int num_procs){
+    
+}
 
 int main(int argc, char **argv) {
-  int num_procs, my_id, msg, buff;
+  int num_procs, my_id, sum, buff;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    sum = my_id + 1;
     if (my_id == 0) {
-      printf("Ingrese un mesage para mandar: \n");
-      scanf("%d",&buff);
-      msg = buff;
-      MPI_Send(&buff, 1, MPI_INT, (my_id + 1) % num_procs, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < num_procs - 1; i++){
+      printf("Soy el proceso %d, mi suma actual es %d y estoy en la iteración %d\n", my_id, sum, i);
+      MPI_Send(&sum, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
       MPI_Recv(&buff, 1, MPI_INT, num_procs - 1, 0, MPI_COMM_WORLD, NULL);
-      if (buff == msg)
-        printf("Finalizo.\n");
-      else
-        printf("Mensaje erroneo.\n");
+      sum += buff;
+      printf("Total de la suma %d, en la iteración %d, id %d\n", sum, i, my_id);
+    } 
+
     } else {
-      MPI_Recv(&buff, 1, MPI_INT, (my_id - 1) % num_procs, 0, MPI_COMM_WORLD, NULL);
-      printf("Soy el proceso %d, recivi el mensaje %d de %d, se lo mando a %d.\n", my_id, buff , (my_id - 1) % num_procs,  (my_id + 1) % num_procs);
-      MPI_Send(&buff, 1, MPI_INT, (my_id + 1) % num_procs, 0, MPI_COMM_WORLD);
+      for (int i = 0; i < num_procs - 1; i++){
+        printf("Soy el proceso %d, mi suma actual es %d y estoy en la iteración %d\n", my_id, sum, i);
+        MPI_Send(&sum, 1, MPI_INT, (my_id + 1) % num_procs, 0, MPI_COMM_WORLD);
+        MPI_Recv(&buff, 1, MPI_INT,  (my_id - 1) % num_procs, 0, MPI_COMM_WORLD, NULL);
+        sum += buff;
+      printf("Total de la suma %d, en la iteración %d, id %d\n", sum, i, my_id);
+      } 
     }
+    printf("Soy el id %d y mi suma quedó %d\n", my_id, sum);
     MPI_Finalize();
     return 0;
 }
