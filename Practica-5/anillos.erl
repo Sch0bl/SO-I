3889bd0c2ed3842f9530ae1e1eb5873a43781bc1
Luciano Scola <lucianoluigiscola@gmail.com>
2023-06-24 00:18:13 -0300

    Anillos terminado

--- Practica-5/anillos.erl
+++ Practica-5/anillos.erl
@@ -1,65 +1,108 @@
 -module(anillos).
--export([start/1, close/1, get_sum/2]).
+-export([start/1, close/0, get_sum/1]).
 
-start(N) -> start_aux(N, N, maps:new()).
+%procs (NroId, Sum, Size, Counter, Map).
+%NroId. Int.
+%Sum. Int variable que guarda la suma.
+%Size. Int Cantidad de procesos.
+%Map. los procesos están guardados en un mapa.
 
-start_aux(0, _, Map) ->
-  List = maps:values(Map),
-  lists:map(fun(Pid) -> Pid ! {init, Map} end, List),
-  Map;
-start_aux(N, S, Map) ->
-  Pid = spawn(fun() -> procs(N - 1, N - 1, S - 1, S, 0, Map) end),
-  NewMap = maps:put(N - 1, Pid, Map),
-  start_aux(N - 1, N, NewMap).
-
-get_sum(Map, NroId) ->
-  Pid = maps:get(NroId, Map),
-  Pid ! {self(), get},
+%Caso Inicial, espera a que todos los procesos sean creados.
+%Una vez recibida la señal init, con TheMap con todos los procesos,
+%Manda al proceso siguiente su NroId para computar la 
+%suma de todos.
+procs(NroId, Sum, Size, Size, _) ->
   receive
-    {_, Sum} -> Sum
-  end.
-
-close(Map) ->
-  List = maps:values(Map),
-  lists:map(fun(Pid) -> Pid ! kill end, List),
-  ok.
-
-procs(NroId, Sum, Size, Counter, 0, _) ->
-  receive
-    {init, TheMap} -> procs(NroId, Sum, Size, Counter, 1, TheMap);
-    kill -> ok 
+    {init, TheMap} -> 
+      NextPid = maps:get((NroId + 1) rem Size, TheMap),
+      NextPid ! {self(), NroId},
+      procs(NroId, Sum, Size, Size - 1, TheMap)
   end;
-procs(NroId, Sum, Size, Counter, 2, Map) ->
+
+%Caso Base: La suma fue computada, espera la señal get para
+%poder retornar el resultado.
+%Con la señal kill se termina el proceso
+procs(NroId, Sum, Size, 0, Map) ->
   receive
     {From, get} -> 
       From ! {self(), Sum},
-      procs(NroId, Sum, Size, Counter, 2, Map) 
+      procs(NroId, Sum, Size, 0, Map);
+    kill -> ok 
   end;
-procs(0, Sum, Size, Counter, 1, Map) ->
+
+%Recursivo: Matching NroId == 0.
+%Es el caso en el que NroId es 0, ya que NroId - 1 es negativo
+procs(0, Sum, Size, Counter, Map) ->
   NextPid = maps:get(1, Map),
   PrevPid = maps:get(Size - 1, Map),
-  NextPid ! {self(), Sum},
   receive
-    {PrevPid, ToSum} -> procs(0, Sum + ToSum, Size, Counter - 1, 1, Map)
+    {PrevPid, ToSum} -> 
+      NextPid ! {self(), ToSum},
+      procs(0, Sum + ToSum, Size, Counter - 1,Map)
   end;
-procs(Size, Sum, Size, Counter, 1, Map) ->
-  NextPid = maps:get(0, Map),
-  PrevPid = maps:get(Size - 2, Map),
-  NextPid ! {self(), Sum},
-  receive
-    {PrevPid, ToSum} -> procs(Size, Sum + ToSum, Size, Counter - 1, i, Map)
-  end;
-procs(NroId, Sum, Size, Counter, 1, Map) ->
-  NextPid = maps:get(NroId + 1, Map),
+
+%Recursivo: 
+%Espera la señal del proceso anterior, cunado resive el sumando
+%se la manda al proceso siguinte llama recursivamente la fución
+%con la suma.
+procs(NroId, Sum, Size, Counter, Map) ->
+  NextPid = maps:get((NroId + 1) rem Size, Map),
   PrevPid = maps:get(NroId - 1, Map),
-  NextPid ! {self(), Sum},
   receive
-    {PrevPid, ToSum} -> procs(NroId, Sum + ToSum, Size, Counter - 1, 1, Map)
-  end;
-procs(NroId, Sum, Size, 0, 1, Map) ->
-  NextPid = maps:get(NroId + 1, Map),
-  PrevPid = maps:get(NroId - 1, Map),
-  NextPid ! {self(), Sum},
-  receive
-    {PrevPid, ToSum} -> procs(NroId, Sum + ToSum, Size, 0, 2, Map)
+    {PrevPid, ToSum} -> 
+      NextPid ! {self(), ToSum},
+      procs(NroId, Sum + ToSum, Size, Counter - 1, Map)
   end.
+
+%Caso base: Como los todos los procesos fueron creados los inicia
+start_proc(0, _, Map) ->
+  maps:map(fun(_,Pid) -> Pid ! {init, Map} end, Map),
+  Map;
+
+%Recursion: Genera un proceso, lo agrega al mapa
+start_proc(N, S, Map) ->
+  Pid = spawn(fun() -> procs(N - 1, N - 1, S, S, Map) end),
+  NewMap = maps:put(N - 1, Pid, Map),
+  start_proc(N - 1, S, NewMap).
+
+%Servidor que mantiene activo el mapa de procesos
+server(Map) ->
+  receive
+    %Obtención de sumas.
+    {From, {get, NroId}} ->
+      Pid = maps:get(NroId, Map),
+      Pid ! {srv, get},
+      receive
+        {Pid, Sum} -> From ! {srv, Sum}
+      end,
+      server(Map);
+    %Cierre del servidor.
+    {From, close} ->
+      maps:map(fun(_, Pid) -> Pid ! kill end, Map),
+      From ! {srv, ok}
+  end.
+
+%Inicia el servidor que mantiene el mapa de pocesos.
+start(N) -> 
+  Map = start_proc(N, N, maps:new()),
+  register(srv, spawn(fun() -> server(Map) end)),
+  ok.
+
+%start_proc(NroId, Size, Map)
+%NroProc es el número de proceso a crear
+%Size es la cantidad de procesos en total
+
+%Obtiene la suma del proceso.
+get_sum(NroId) ->
+  srv ! {self(), {get, NroId}},
+  receive
+    {srv, Sum} -> Sum
+  end.
+
+%Cierra el servidor.
+close() ->
+  srv ! {self(), close},
+  receive
+    {srv, ok} -> ok 
+  end.
+
